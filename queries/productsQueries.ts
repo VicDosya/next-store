db35d4fcffd81438b67a9e9a1173e25f3cdac0e3
dosya <vicdosya@gmail.com>
2023-07-26 13:51:51 +0300

    move queries out of products

--- queries/productsQueries.ts
+++ queries/productsQueries.ts
@@ -1,5 +1,33 @@
 import { gql } from "graphql-request";
 
+export const productsQuery = gql`
+  query Products {
+    products(first: 10) {
+      edges {
+        node {
+          id
+          title
+          handle
+          tags
+          priceRange {
+            minVariantPrice {
+              amount
+            }
+          }
+          images(first: 1) {
+            edges {
+              node {
+                transformedSrc
+                altText
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const singleProductHandleQuery = gql`
   {
     products(first: 4) {
