5f022594703ebfc9f7b82b549904b1afc761b5b7
dosya <vicdosya@gmail.com>
2023-07-28 17:58:04 +0300

    Add preview parameter

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ import { productsQuery } from '../queries/productsQueries';
 import ProductsProvider from '../components/ProductsContext';
 import { getStoryblokContentVersion } from '../utils/helpers';
 
-export default function Home({ story, products }: any) {
+export default function Home({ story, products, preview }: any) {
   //Enable the Visual Editor
-  story = useStoryblokState(story);
+  story = useStoryblokState(story, preview);
 
   return (
     <>
@@ -18,7 +18,7 @@ export default function Home({ story, products }: any) {
   )
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ preview = false }) {
   try {
     const storyblokApi = getStoryblokApi();
 
@@ -26,8 +26,9 @@ export async function getStaticProps() {
     let { data }: any = await storyblokApi.get("cdn/stories/home", {
       version: getStoryblokContentVersion(),
       cv: +new Date(),
+      ...(preview ? { version: "draft" } : {}),
     });
-    // Recieve shopify data
+    // Recieve shopify data.
     const shopifyData: any = await storefront(productsQuery);
 
     return {
@@ -37,6 +38,7 @@ export async function getStaticProps() {
         key: data ? data.story.id : false,
         //Shopify
         products: shopifyData.data,
+        preview,
       },
       revalidate: 3600,
     }
